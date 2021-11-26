b31fd36a62073e93b6ce090dc3727f03bf50bfe0
Francesco Novy <francesco@fabscale.com>
2021-11-26 10:58:45 +0100

    Initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,7 @@
-'use strict';
-
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true,
-    },
-  },
-  plugins: ['ember'],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended',
-    'plugin:prettier/recommended',
-  ],
-  env: {
-    browser: true,
-  },
-  rules: {},
+  ...require('fabscale-eslint-config/lib/ember'),
+
   overrides: [
     // node files
     {
@@ -33,21 +15,14 @@ module.exports = {
         './blueprints/*/index.js',
         './config/**/*.js',
         './tests/dummy/config/**/*.js',
+        './lib/**/*.js',
       ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-      env: {
-        browser: false,
-        node: true,
-      },
-      plugins: ['node'],
-      extends: ['plugin:node/recommended'],
+      ...require('fabscale-eslint-config/lib/node'),
     },
     {
-      // Test files:
+      // test files:
       files: ['tests/**/*-test.{js,ts}'],
-      extends: ['plugin:qunit/recommended'],
+      ...require('fabscale-eslint-config/lib/ember-tests'),
     },
   ],
 };
